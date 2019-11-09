ae10f5ecd57071e506e8ac7ac0c1624c14435e2c
1092630615@qq.com <1246755925@qq.com>
2019-11-09 19:56:23 +0800

    shuibian3

--- kits/kits.js
+++ kits/kits.js
@@ -10,4 +10,26 @@ function randomInt(n, m) {
     var b = randomInt(0, 255);
     var color = 'rgb(' + r + ',' + g + ',' + b + ')';
     return color;
+    // 十六进制方法1
+  function randomColor(){
+    var  arr =['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f',]
+  var color =['#'];
+  for(var i=0; i<6;i++){
+    var r = Math.floor(Math.random()*arr.length);
+    color.push(arr[r]);
+  }
+  return color.join('');
+  }
+  console.log(randomColor());
+  document.body.style.backgroundColor=randomColor();
+  // 十六进制第二种方法
+  function randomColor1(){
+    var color=['#'];
+    for(var i=0; i<6;i++){
+      var r =Math.floor(Math.random()*16).toString(16);
+    }
+    return color.join('');
+  }
+  console.log(randomColor1());
+  document.body.style.backgroundColor=randomColor1();
   }
